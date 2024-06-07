7a36cc59103855d9b04b66f2b61b3c110645a56d
Angelo Echano <98934937+aechano@users.noreply.github.com>
2024-06-08 04:49:37 +0800

    SplashScreen

--- app/src/main/java/com/capstone/merkado/Screens/Account/SignOut.java
+++ app/src/main/java/com/capstone/merkado/Screens/Account/SignOut.java
@@ -6,6 +6,7 @@ import androidx.cardview.widget.CardView;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.capstone.merkado.Application.Merkado;
@@ -24,6 +25,16 @@ public class SignOut extends AppCompatActivity {
         // initialize this activity's screen.
         merkado = Merkado.getInstance();
         merkado.initializeScreen(this);
+
+        // Set the activity to full-screen mode
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         // find views
         CardView signOut = findViewById(R.id.sign_out);
---
+++ app/src/main/java/com/capstone/merkado/Screens/LoadingScreen/LoadingScreen.java
@@ -0,0 +1,100 @@
+package com.capstone.merkado.Screens.LoadingScreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.capstone.merkado.Application.Merkado;
+import com.capstone.merkado.DataManager.DataFunctions;
+import com.capstone.merkado.Objects.Account;
+import com.capstone.merkado.R;
+import com.capstone.merkado.Screens.MainMenu.MainMenu;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    private Merkado merkado;
+    private ProgressBar progressBar;
+    private Timer timer;
+    private int i=0;
+    private boolean isProcess1Completed = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spl_loading_screen);
+
+        // initialize this activity's screen.
+        merkado = Merkado.getInstance();
+        merkado.initializeScreen(this);
+
+        // Set the activity to full-screen mode
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        progressBar = findViewById(R.id.simpleProgressBar);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (i<100){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(i);
+                        }
+                    });
+                    i++;
+
+                    // Call process1() in a background thread
+                    if (i == 50 && !isProcess1Completed) {
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                process1();
+                                isProcess1Completed = true;
+                            }
+                        }).start();
+                    }
+
+                } else {
+                    timer.cancel();
+
+                    // Ensure process1() has completed before starting the next activity
+                    if (isProcess1Completed) {
+                        // go to next activity: Main Menu
+                        startActivity(new Intent(LoadingScreen.this, MainMenu.class));
+                        finish();
+                    }
+                }
+            }
+        },0, 50);
+    }
+
+    /**
+     * 1st process of loading screen. This retrieves the account logged in from the SharedPref.
+     */
+    private void process1() {
+        // get the signed in from sharedpref.
+        Account account = DataFunctions.getSignedIn(getApplicationContext());
+        if (account == null) return; // stop the function here if sign in is required.
+
+        // update the account in the application class.
+        merkado.setAccount(account);
+
+        // update the account logged in from the SharedPref
+        DataFunctions.signInAccount(getApplicationContext(), account);
+    }
+}

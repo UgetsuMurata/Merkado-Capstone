8335100772cfb86d70ebd30eca7ca9c797633a05
MurataUgetsu <yanezakai@gmail.com>
2024-06-11 00:22:26 +0800

    StoryMode layout

---
+++ app/src/main/java/com/capstone/merkado/Screens/Game/StoryMode.java
@@ -0,0 +1,16 @@
+package com.capstone.merkado.Screens.Game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.capstone.merkado.R;
+
+public class StoryMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gam_story_mode);
+    }
+}
